d0f0bd75455b85dd8431f9948ae08e4db8a84409
priyaKumari-1 <priyasharmacodm@gmail.com>
2026-04-23 17:26:49 +0530

    fix error

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "36272dcd-c5a8-4509-87c3-fec26af76f67",
     authority: "https://login.microsoftonline.com/cec2262d-9594-498e-94ed-d6ffcef6304e",
-    redirectUri: "http://localhost:5173",
+    redirectUri: "https://codmsoftware.co.uk/myKoda",
   },
 };
 
