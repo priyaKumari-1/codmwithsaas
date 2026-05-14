2405186f5d65143d5aa9f869a442042377cd4067
Hardiksharma07 <156889739+Hardiksharma07@users.noreply.github.com>
2026-05-14 13:16:39 +0100

    Enhance SEO with additional meta tags
    Added Open Graph and Twitter meta tags for SEO.

--- src/SeoData/SEO.jsx
+++ src/SeoData/SEO.jsx
@@ -13,8 +13,12 @@ function SEO({ title, description, url, image , keywords }) {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image}/>
+      <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
-      
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="author" content="CODM Software Limited" />
     </Helmet>
   );
 }
